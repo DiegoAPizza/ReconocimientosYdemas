57f3f0dc56f0a8dfc355cf120c1030f93757c7b6
Diego A.Pizza <79482954+DiegoAPizza@users.noreply.github.com>
2023-12-25 16:54:00 -0300

    contornos

--- contorno.py
+++ contorno.py
@@ -1,18 +1,18 @@
-import cv2
+import cv2 as cv
 
-imagen=cv2.imread('contorno.jpg')
+imagen=cv.imread('contorno.jpg')
 
-gris=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
+gris=cv.cvtColor(imagen,cv.COLOR_BGR2GRAY)
 
-_,umbral=cv2.threshold(gris,100,255,cv2.THRESH_BINARY)
+_,umbral=cv.threshold(gris,100,255,cv.THRESH_BINARY)
 
-contorno,jeraquia=cv2.findContours(umbral,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+contorno,jeraquia=cv.findContours(umbral,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
 
-
+cv.drawContours(imagen,contorno,-1,(251,60,50),3)
 
 #Mostrar
-cv2.imshow("Imagen Original ",imagen)
-cv2.imshow("Imagen con escala de grises ",gris)
-cv2.imshow("Imagen con aplicacion de Umbral",umbral)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.imshow("Imagen Original ",imagen)
+#cv.imshow("Imagen con escala de grises ",gris)
+#cv.imshow("Imagen con aplicacion de Umbral",umbral)
+cv.waitKey(0)
+cv.destroyAllWindows()
